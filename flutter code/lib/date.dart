75bb1fccb8ac8c0caeaa3a0bb7171aaddbbd185e
阿吉 <zxzx37913312@gmail.com>
2020-10-22 19:33:28 +0800

    TTTTTT

--- flutter code/lib/date.dart
+++
@@ -1,8 +0,0 @@
-class Date {
-  String year;
-  String month;
-  String day;
-  String nowtime;
-  int id;
-  Date({this.year,this.month,this.day,this.nowtime,this.id});
-}
--- flutter code/lib/emojiCount.dart
+++
@@ -1,8 +0,0 @@
-class EmojiCount {
-  int angry;
-  int sad;
-  int normal;
-  int happy;
-  int joy;
-  EmojiCount(this.angry, this.sad, this.normal, this.happy, this.joy);
-}
